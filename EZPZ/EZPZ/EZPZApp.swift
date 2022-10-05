437ecb335f0ed3753f2ee414ff3f51daa7a54936
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-10-06 04:20:15 +0900

    :tada: EZPZ Refactoring Start

---
+++ EZPZ/EZPZ/EZPZApp.swift
@@ -0,0 +1,20 @@
+//
+//  EZPZApp.swift
+//  EZPZ
+//
+//  Created by 이주화 on 2022/10/06.
+//
+
+import SwiftUI
+
+@main
+struct EZPZApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
