4e51bfaf7c0bd14dcba4651ec737430d5c51afa1
RuyHa <code.ruyha@gmail.com>
2022-10-17 14:11:53 +0900

    [Feat] 폰트 설정 완료 (폰트, 사이즈, 컬러) 적용 완료

--- EZPZ/EZPZ/Extensions/Text+.swift
+++ EZPZ/EZPZ/Extensions/Text+.swift
@@ -7,85 +7,74 @@
 
 import SwiftUI
 
-extension Font {
-    static let name = Font.custom("GillSans-UltraBold", size: 14)
-    static let location = Font.custom("GillSans-SemiBold", size: 10)
-    static let date = Font.custom("GillSans-UltraBold", size: 16)
-    static let price = Font.custom("GillSans-SemiBoldItalic", size: 12)
-}
-
-/*
- === SpoqaHanSans-Regular
- === SpoqaHanSans-Bold
- */
 extension Text {
     func basicTitle1() -> Text {
-        self.font(Font.custom("ppap", size: 34))
+        self.font(Font.custom("SpoqaHanSans-Bold", size: 34))
             .foregroundColor(.grayScale1)
     }
     
     func limeTitle1() -> Text {
-        self.font(Font.custom("ppap", size: 34))
+        self.font(Font.custom("SpoqaHanSans-Bold", size: 34))
             .foregroundColor(.ezpzLime)
     }
     
     func basicTitle2() -> Text {
-        self.font(Font.custom("ppap", size: 28))
+        self.font(Font.custom("SpoqaHanSans-Bold", size: 28))
             .foregroundColor(.grayScale1)
     }
     
     func limeTitle2() -> Text {
-        self.font(Font.custom("ppap", size: 28))
+        self.font(Font.custom("SpoqaHanSans-Bold", size: 28))
             .foregroundColor(.ezpzLime)
     }
     
     func pinkTitle3() -> Text {
-        self.font(Font.custom("ppap", size: 24))
+        self.font(Font.custom("SpoqaHanSans-Bold", size: 24))
             .foregroundColor(.ezpzPink)
     }
     
     func limeTitle4() -> Text {
-        self.font(Font.custom("ppap", size: 18))
+        self.font(Font.custom("SpoqaHanSans-Bold", size: 18))
             .foregroundColor(.ezpzLime)
     }
     
     func basicHeadline() -> Text {
-        self.font(Font.custom("ppap", size: 17))
+        self.font(Font.custom("SpoqaHanSans-Bold", size: 17))
             .foregroundColor(.grayScale1)
     }
     
     func limeHeadline() -> Text {
-        self.font(Font.custom("ppap", size: 17))
+        self.font(Font.custom("SpoqaHanSans-Bold", size: 17))
             .foregroundColor(.ezpzLime)
     }
     
     func basicBody() -> Text {
-        self.font(Font.custom("ppap", size: 17))
+        self.font(Font.custom("SpoqaHanSans-Regular", size: 17))
             .foregroundColor(.grayScale1)
     }
     
     func grayScale3Body() -> Text {
-        self.font(Font.custom("ppap", size: 17))
+        self.font(Font.custom("SpoqaHanSans-Regular", size: 17))
             .foregroundColor(.grayScale3)
     }
     
     func grayScale4Body() -> Text {
-        self.font(Font.custom("ppap", size: 17))
+        self.font(Font.custom("SpoqaHanSans-Regular", size: 17))
             .foregroundColor(.grayScale4)
     }
     
     func grayScale3Caption() -> Text {
-        self.font(Font.custom("ppap", size: 13))
+        self.font(Font.custom("SpoqaHanSans-Regular", size: 13))
             .foregroundColor(.grayScale3)
     }
     
     func limeCaption() -> Text {
-        self.font(Font.custom("ppap", size: 13))
+        self.font(Font.custom("SpoqaHanSans-Regular", size: 13))
             .foregroundColor(.ezpzLime)
     }
     
     func grayScale3Headline() -> Text {
-        self.font(Font.custom("ppap", size: 17))
+        self.font(Font.custom("SpoqaHanSans-Bold", size: 17))
             .foregroundColor(.grayScale3)
     }
 }
